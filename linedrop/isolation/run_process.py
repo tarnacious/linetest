9cebdd9358c6c2b9d73a0848cc5f09b85f8932aa
Tarn Barford <tarn@tarnbarford.net>
2015-06-14 18:54:19 +0200

    Increase timeout

--- linedrop/isolation/run_process.py
+++ linedrop/isolation/run_process.py
@@ -5,16 +5,11 @@ def run_process(fn):
     q = Queue()
 
     def run():
-        print "START"
         q.put(fn())
-        print "FINISH"
-
 
     p = Process(target=run)
     p.start()
-    print "STARTED"
-    p.join(timeout=2)
-    print "OH?"
+    p.join(timeout=60)
     if not q.empty():
         return q.get()
     else:
